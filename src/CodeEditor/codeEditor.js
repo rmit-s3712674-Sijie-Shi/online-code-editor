2c565cb3173d031ecfabfb0cf9094b2f330c8788
rikusrocks <rikusrocks@gmail.com>
2023-04-27 17:54:54 +1000

    some css

--- src/CodeEditor/codeEditor.js
+++ src/CodeEditor/codeEditor.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from "react";
 import ReactCodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { getToken, testCode } from "./service";
+import "./codeEditor.css"
 
 const CodeEditor = () => {
     const [codeState, setCodeState] = useState('')
@@ -17,20 +18,21 @@ const CodeEditor = () => {
         })
     }, [codeState])
 
-    return(
-        <>
-            <ReactCodeMirror value="" 
-            extensions={[javascript({typescript: true})]} 
-            onChange={onChange} 
-            placeholder={"enter your code here"}
-            height="400px"
-            width="500px">
+    return (
+        <div className="editorContainer">
+            <ReactCodeMirror value=""
+                extensions={[javascript({ typescript: true })]}
+                onChange={onChange}
+                placeholder={"enter your code here"}
+                height="400px"
+                width="500px"
+            >
             </ReactCodeMirror>
             <div>
-            {result ? "Your result: " + result : "Your result: "}
+                {result ? "Your result: " + result : "Your result: "}
             </div>
             <button onClick={() => submitCode()}>submit</button>
-        </>
+        </div>
     )
 }
 
--- src/Container/container.js
+++ src/Container/container.js
@@ -1,10 +1,11 @@
 import React from "react";
 import CodeEditor from "../CodeEditor/codeEditor";
 import Question from "../Question/question";
+import './container.css'
 
 const Container = () => {
     return(
-        <div>
+        <div className="container">
             <Question></Question>
             <CodeEditor></CodeEditor>
         </div>
