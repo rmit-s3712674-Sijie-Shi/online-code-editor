7a279d47001811a4ea7ce1757bb4c97da5f28c22
rikusrocks <rikusrocks@gmail.com>
2023-04-28 21:47:07 +1000

    open test detail, back to test list

--- src/Question/question.js
+++ src/Question/question.js
@@ -8,20 +8,36 @@ import { useCallback } from "react";
 
 const Question = () => {
     const [queue, setQueue] = useState([])
+    const [questionSelected, setQuestionSelected] = useState()
+    const [selected, setSelected] = useState(false)
+    
     useEffect(() => {
       const que = sessionStorage.getItem("queue").split(',')
       setQueue(que)
     },[])
 
 
+    const handleBack = useCallback(() => {
+        console.log("back")
+        setQuestionSelected(null)
+        setSelected(false) 
+    }, [])
+
+    const handleSelectQuestion = useCallback((id) => {
+        setQuestionSelected(id)
+        setSelected(true)  
+    }, [])
 
     return(
         <div className="questionContainer">
             <div className="cardContainer">
                 <div className="title">Question list</div>
                 <div className="qCard">
-                    {queue.map((res) => (
-                        <QuestionCard {...data[res]}></QuestionCard>
+                    { selected ? 
+                    <QuestionCard {...data[questionSelected]} key={data[questionSelected].id} handleBack={handleBack}></QuestionCard>
+                    :
+                    queue.map((res) => (
+                        <QuestionCard {...data[res]} key={data[res].id} changeQuestion={handleSelectQuestion}></QuestionCard>
                     ))}
                 </div>
             </div>
--- src/Question/questionCard.js
+++ src/Question/questionCard.js
@@ -1,12 +1,49 @@
 import React from "react";
+import { useState } from "react";
 
-const QuestionCard = ({ id, title, detail, hint, result}) => {
-    return(
-        <div id={id} key={id}>
+const QuestionCard = ({ id, title, detail, hint, result, changeQuestion, handleBack }) => {
+
+    const [show, setShow] = useState(false)
+
+    const clickTask = () => {
+        changeQuestion(id)
+        setShow(true)
+    }
+
+    const back = () => {
+        handleBack()
+        setShow(false)
+    }
+
+    return (
+        <>
+        {show? 
+        <div id={id} className="card">
+            <div className="left" onClick={back}>
+                 &lt;back
+            </div>
             <div >
+                Question:
                 {title}
             </div>
-        </div>
+            <div>
+                Description: 
+                {detail}
+            </div>
+            <div>
+                Hint:
+                {hint}
+            </div>
+        </div> 
+        :             
+        <div id={id} className="card hover" onClick={clickTask}>
+                <div >
+                    {title}
+                </div>
+            </div>}
+
+        </>
+
     )
 }
 
