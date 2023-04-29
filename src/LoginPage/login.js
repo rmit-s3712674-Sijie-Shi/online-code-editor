f171a51e86616bdc33adbc39f78994b16e6c1e11
rikusrocks <rikusrocks@gmail.com>
2023-04-29 14:11:24 +1000

    add router, login page

--- src/LoginPage/login.js
+++ src/LoginPage/login.js
@@ -1,8 +1,40 @@
 import React from "react";
+import './login.css'
 
 const Login = () => {
     return (
-        <div></div>
+        <body>
+        <div className="loginContainer">
+            <div className="form-box">
+                <div className="register-box hidden">
+                    <h1>register</h1>
+                    <input type="text" placeholder="username"></input>
+                    <input type="email" placeholder="email"></input>
+                    <input type="password" placeholder="password"></input>
+                    <input type="password" placeholder="confirm your password"></input>
+                    <button>register</button>
+                </div>
+                <div className="login-box">
+                    <h1>login</h1>
+                    <input type="text" placeholder="username"></input>
+                    <input type="password" placeholder="password"></input>
+                    <button>login</button>
+                </div>
+            </div>
+            <div className="con-box left-box">
+                <h2>welcome</h2>
+                <p>code test</p>
+                <p>already have an account? </p>
+                <button id="login">login</button>
+            </div>
+            <div className="con-box right-box">
+                <h2>welcome</h2>
+                <p>code test</p>
+                <p>don't have an account? </p>
+                <button id="register">register</button>
+            </div>
+        </div>
+        </body>
     )
 }
 
